c83259c0ee0233218de75c6012f751e0d667143b
Medidali <m.mohamedidali.com>
2023-06-16 17:54:26 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,17 @@ int main(void)
 {
 	int c;
 
-	for (c = '0'; c < '9'; c++)
-       
-	        putchar(c',');
-		putchar(' ');
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
 		}
-        putchar('9');
+	}
+
 	putchar('\n');
 
 	return (0);
