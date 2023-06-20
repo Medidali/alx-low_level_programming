755cbe6546743969f2cc415d272cc66a8e753560
Medidali <m.mohamedidali.com>
2023-06-20 11:25:16 -0700

    this is C - Functions, nested loops projct

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,16 @@
 #include "main.h"
-
-
+/**
+  * print_alphabet_10 - Make the alphabet
+  *
+  * Return: void
+  */
 void print_alphabet_x10(void)
 {
 char letr;
- int i=0;
- 
- while(i < 10)
+int i = x;
+while (i < 10)
 {
- for (letr = 'a'; letr <= 'z'; letr++)
+for (letr = 'a'; letr <= 'z'; letr++)
 {
 _putchar(letr);
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c~
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+void print_alphabet_x10(void)
+{
+char letr;
+ int i=0;
+ 
+ while(i < 10)
+{
+ for (letr = 'a'; letr <= 'z'; letr++)
+{
+_putchar(letr);
+}
+_putchar('\n');
+i++;
+}
