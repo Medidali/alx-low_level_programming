f7531e11784fbf7082bd6e7b8365b7612a0c49b0
Medidali <m.mohamedidali.com>
2023-06-20 15:18:41 -0700

    this is Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,26 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
   * print_sign - Prints the sign of a number
   * @n: The number to be checked
   *
   * Return: 1 for positive num, -1 for negative num or zero for anything else
   */
-
 int print_sign(int n)
 {
-if (n > 0)
-return (1);
-_putchar('+');
-else (n == 0)
-return (0);
-_putchar('0');
-else
-return (-1);
-_putchar('-');
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
