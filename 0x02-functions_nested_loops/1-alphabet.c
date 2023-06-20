135d8d8ccdca16a331c2feeea63ff6135e609474
Medidali <m.mohamedidali.com>
2023-06-20 10:48:52 -0700

    this is C - Functions, nested loops projct

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,28 @@
-#include <std.h>
 #include "main.h"
-
-
+/**
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
 
 void print_alphabet(void)
 {
-  char letr;
-
-  for(letr = 'a'; letr <= 'z'; letr++)
-    {
-      _putchar(letr);
-    }
-  _putchar('\n');
+char letr;
+for (letr = 'a'; letr <= 'z'; letr++)
+{
+_putchar(letr);
 }
+_putchar('\n');
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
-  print_alphabet();
-  return (0);
+print_alphabet();
+return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c~
@@ -0,0 +1,21 @@
+#include <std.h>
+#include "main.h"
+
+
+
+void print_alphabet(void)
+{
+  char letr;
+
+  for(letr = 'a'; letr <= 'z'; letr++)
+    {
+      _putchar(letr);
+    }
+  _putchar('\n');
+}
+
+int main(void)
+{
+  print_alphabet();
+  return (0);
+}
