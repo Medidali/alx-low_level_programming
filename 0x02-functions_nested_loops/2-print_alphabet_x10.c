2cdd1e7601a206eb4a15656db27a1a73dc0b28ce
Medidali <m.mohamedidali.com>
2023-06-20 11:36:03 -0700

    this is C - Functions, nested loops projct

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
+
 /**
-  * print_alphabet_10 - Make the alphabet
+  * print_alphabet_x10 - Make alphabet x10 times
   *
   * Return: void
   */
+
 void print_alphabet_x10(void)
 {
 char letr;
