aab096f0a1638d44e2ce7bf0f39fb00286d15b4e
Medidali <m.mohamedidali.com>
2023-06-20 08:36:35 -0700

    the C functuins

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /*
   * main - Entry point
   *
   * Return: Always 0
   */
-
 int main(void)
 {
+  /*this functuin for display*/
 char contener[] = "_putchar";
 int i;
 for (i = 0; contener[i] != '\0'; i++)
