7afbb3088d382e545efc14b75004781485b46427
Medidali <m.mohamedidali.com>
2023-06-23 12:59:38 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,41 +1,20 @@
+#include <stdio.h>
 #include "main.h"
-/**
- * largestPrimeFactor - prints the largest prime factor of @number
- *@number:.input integer parameter
- * Return: always largestFactor
- */
-long long largestPrimeFactor(long long number)
-{
-longlong largestFactor = 2;
-
-if (number % 2 == 0)
-{
-number /= 2;
-}
-long long factor = 3;
-
-for (long long factor = 3; factor * factor <= number; factor += 2)
-{
-while (number % factor == 0)
-{
-largestFactor = factor;
-number /= factor;
-}
-}
-
-return (largestFactor);
-}
-
 /**
  * main - prints the largest prime factor of 612852475143
  * Bwave ICT / Bright Daniel
  * Return: always 0
  **/
+
 int main(void)
 {
-long long number = 612852475143;
-long long result = largestPrimeFactor(number);
-printf("The largest prime factor of %lld is: %lld\n", number, result);
+	unsigned long int i, n = 612852475143;
 
-return (0);
+	for (i = 3; i < 782849; i = i + 2)
+	{
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
 }
