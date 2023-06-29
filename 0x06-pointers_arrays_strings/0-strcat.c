c96946c186d6922cc90e9acebc156eb96663ec30
Medidali <m.mohamedidali.com>
2023-06-29 05:13:39 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_strcat - function that concatenates two strings.
+ *@dest: parmetre passed to function
+ *@src: parametre passed to function
+ *
+ *Return: return the resultatbof concatenation
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+stract(*dest, *src);
+return (*dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h~
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#define MAIN_H
+#ifndef MAIN_H
+
+char _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#fendif; // MAIN_H
