8ffe1db9361572fb1881376f262284d52e7851d3
Medidali <m.mohamedidali.com>
2023-06-29 14:32:37 -0700

    0x06  C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strcat - fuction wich concatainate two string
+ * @dest: parametre 1 passed to funct
+ * @src:parametre 2 passed to funct 
+ *@n: parametre 3 passed to funct 
+ *
+ * Return:char
+ */
+
+
+
+
+
+
+char *_strcat(char *dest, char *src, int n)
+{
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+int i = 0;
+	while (i != n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	*dest = '\0';
+	return (s);
+}
