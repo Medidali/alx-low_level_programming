a9b1cac403fa77ccc0136978e63b0156ed6aa2dd
Medidali <m.mohamedidali.com>
2023-07-03 08:34:10 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,11 +13,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i = 0;
-while (i < n)
-{
-dest[i] = src[i];
-i++;
-}
-return (dest);
+  int i = 0;
+  while (i < n)
+    {
+      dest[i] = src[i];
+      i++;
+    }
+  return (dest);
 }
