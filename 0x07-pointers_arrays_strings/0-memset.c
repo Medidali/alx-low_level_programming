7c758fb1cad7f935cb1ffa549b560e12b5031521
Medidali <m.mohamedidali.com>
2023-07-03 07:16:40 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,26 @@
 #include "main.h"
 
 /**
- *_memset - that fills memory with a constant byte
- *@s: parametre char
- *@b: parametre conctant
- *@n: parametre size
+ * _memset - function fill the first @n bytes of the memory area pointed
+ * to by @s with the constant byte @b
  *
- *Return: Return char
+ * @n: bytes of the memory area pointed to by @s
+ *
+ * @s: with the constant byte @b
+ *
+ * @b: memory area pointer
+ *
+ * Return: a pointer to the memory area @s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0;
-while (i < n)
-{
-s[i] = b;
-i++;
-}
-return (s);
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+	s[i] = b;
+	i++;
+	}
+
+	return (s);
 }
