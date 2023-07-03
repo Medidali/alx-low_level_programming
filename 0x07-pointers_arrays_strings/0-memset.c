42ed71329265ecc64cc78047ffa244be2ff01a0d
Medidali <m.mohamedidali.com>
2023-07-03 06:26:52 -0700

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_memset - that fills memory with a constant byte
+ *@s: parametre char
+ *@b: parametre conctant
+ *@n: parametre size
+ *
+ *Return: Return char
+
+
+
+
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    int i = 0;
+     while(i < n)
+     {
+        s[i] = b; 
+        i++;
+     }
+   return (s);  
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define #MAIN_H
+#ifndef #MAIN_H
+char *_memset(char *s, char b, unsigned int n);
+
+
+
+
+
+
+#endif #MAIN_H
