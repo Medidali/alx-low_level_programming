abd7df1e4f6a56cf23c4fbaace9b4d005f21ff2e
Medidali <m.mohamedidali.com>
2023-07-07 09:41:27 -0700

    0x09. C - Static libraries

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -4,7 +4,6 @@
 
 
 /**
- *
  *  * _putchar - writes the character c to stdout
  *
  *   * @c: The character to print
@@ -13,7 +12,8 @@
  *
  *     * Return: On success 1.
  *
- *      * On error, -1 is returned, and errno is set appropriately*/
+ *      * On error, -1 is returned, and errno is set appropriately
+*/
 
 int _putchar(char c)
 
