cc0b6c98213d8ec763a6718ea3187d8e4568f549
Medidali <m.mohamedidali.com>
2023-07-11 16:45:47 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,37 +1,48 @@
 #include "main.h"
-#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
-*str_concat - function concatenates two strings
-*@s1: parametre char passed to function
-*@s2: parametre char passed to function
-*
-*Return: return string
-*/
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: a pointer to a newly allocated space in memory which
+ * contains the contents of s1, followed by the contents of s2,
+ * and null terminated. NULL on failure
+ */
 char *str_concat(char *s1, char *s2)
 {
-char *concat;
-int len1, len2;
-if ((s1 == NULL) && (s2 == NULL))
-{
-return (NULL);
-}
-if ((s1 != NULL) && (s2 == NULL))
-{
-return (s1);
-}
-if ((s1 == NULL) && (s2 != NULL))
-{
-return (s2);
-}
-len1 = strlen(s1);
-len2 = strlen(s2);
-concat = (char *)(sizeof(char) * (len1 + len2 + 1));
-if (concat == NULL)
-{
-return (NULL);
-}
-strcpy(concat, s1);
-strcat(concat, s2);
-return (concat);
+	int i, j, len1, len2, len;
+	char *result;
 
+	len1 = len2 = 0;
+
+	if (s1 != NULL)
+	{
+		i = 0;
+		while (s1[i++] != '\0')
+			len1++;
+	}
+
+	if (s2 != NULL)
+	{
+		i = 0;
+		while (s2[i++] != '\0')
+			len2++;
+	}
+
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
+
+	return (result);
 }
+
