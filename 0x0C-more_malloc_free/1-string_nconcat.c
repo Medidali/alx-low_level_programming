8130d10c516cac75983b2fd409c3351b72819192
Medidali <m.mohamedidali.com>
2023-07-13 15:53:03 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,17 +12,21 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
 char *s;
 unsigned int i;
 unsigned int len1;
 unsigned int len2;
-
-if ((s1 == NULL) || (s2 == NULL))
+if (s1 == NULL)
 {
-return (NULL);
+s1 = "";
 }
+else
 len1 = strlen(s1);
+if (s2 == NULL)
+{
+s2 = "";
+}
+else
 len2 = strlen(s2);
 s = malloc(len1 + n + 1);
 if (s == NULL)
