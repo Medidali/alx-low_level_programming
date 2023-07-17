eccd8b8344e05302e81316a2697fe421bddf753c
Medidali <m.mohamedidali.com>
2023-07-17 16:39:38 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,12 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ *dog - structure that contain dog information
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+ */
 struct dog
 {
 char *name;
