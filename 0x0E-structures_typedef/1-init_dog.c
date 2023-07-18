db2f82e7b6e1223ca91b95ef1d88171f2954184a
Medidali <m.mohamedidali.com>
2023-07-17 18:03:21 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,21 @@
 #include "dog.h"
-
-
+#include <string.h>
+/**
+ *init_dog - function that initialize a variable of type struct dog
+ *@d: parametre of type struct dog
+ *@name : parametre of type char
+ *@age : parametre of type age
+ *@owner : parametre of type char
+ *
+ *Return : return alwys void.
+ */
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-d.name = name;
-d.age = age;
-d.owner = owner;
-return ();
+strcpy(d->name, name);
+d->age = age;
+strcpy(d->owner, owner);
+return;
 }
