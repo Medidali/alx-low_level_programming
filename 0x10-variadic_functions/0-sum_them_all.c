40a72f4cc2083b708c56b9a5aecca4b2bef4acc0
Medidali <m.mohamedidali.com>
2023-07-20 13:18:46 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,8 +8,8 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int sum = 0;
-int i;
+int sum = 0;
+unsigned int i;
 if (n != 0)
 {
 va_list args;
@@ -19,7 +19,7 @@ for (i = 0; i < n; i++)
 sum += va_arg(args, unsigned int);
 }
 va_end(args);
-retuen sum;
+return sum;
 }
-retuen (0);
+return (0);
 }
