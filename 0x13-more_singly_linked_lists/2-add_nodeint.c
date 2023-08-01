a3883888a9e8a89399061cae69f10cdc7bc91bf5
Medidali <m.mohamedidali.com>
2023-08-01 09:24:56 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,4 +1,4 @@
-#inClude "lists.h"
+#include "lists.h"
 
 /**
  *add_nodeint - function that adds a new node at the beginning of a list_t list.
@@ -8,7 +8,7 @@
  */
 
 
-listintx_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *newNode;
 
