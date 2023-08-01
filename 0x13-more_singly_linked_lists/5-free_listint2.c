a933921d7ee0022d1a4e7fe258cadc78ffd0b444
Medidali <m.mohamedidali.com>
2023-08-01 11:39:58 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,20 +1,18 @@
 #include "lists.h"
-
 /**
  * free_listint2 - Realease the memory allocated for a list
  *
  * @head: A pointer to the first node of the list to free
+ *Return: void
  */
 void free_listint2(listint_t **head)
 {
-
-
 while (*head != NULL)
 {
+if (head == NULL)
+return;
+
 free(*head);
 *head = (*head)->next;
-
 }
-*head = NULL;
-
 }
