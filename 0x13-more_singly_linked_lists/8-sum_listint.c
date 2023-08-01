6ae6b7c3eeccadde643d8e8b43b3dd1aa13e34be
Medidali <m.mohamedidali.com>
2023-08-01 15:01:47 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* sum_listint - returns the sum of all the data (n) of a listint_t list.
+ * @head: pointer to the list.
+ * Return: sum of all data in the list, 0 if the lost is empty.
+*/
+
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+
+if (head == NULL)
+return (0);
+
+while (head != NULL)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
+}
