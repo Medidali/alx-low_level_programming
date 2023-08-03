a3e380db592623bb1c925666eedb42222182bdbf
Medidali <m.mohamedidali.com>
2023-08-03 13:37:44 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,26 +8,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-
 int i;
 unsigned int num = 0;
-int len = strlen(b);
-char c;
 
 if (b == NULL)
 {
 return (0);
 }
 
-for (i = 0; i < len; i++)
+for (i = 0; b[i] != '\0'; i++)
 {
-
-c = b[i];
-if (c != '0' && c != '1')
+if (b[i] != '0' && b[i] != '1')
 
 return (0);
 }
-for (i = 0; i < len; i++)
+
+for (i = 0; b[i] != '\0'; i++)
 {
 num <<= 1;
 
