4828c81bfc2138bed7d0620de7eb93ffe41771f6
Medidali <m.mohamedidali.com>
2023-08-03 12:58:44 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *binary_to_uint - function that convert binary to ui
  *@b : pointer to the string
@@ -7,20 +8,29 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+
 int i;
 unsigned int num = 0;
 int len = strlen(b);
+char c;
 
 if (b == NULL)
+{
 return (0);
+}
 
 for (i = 0; i < len; i++)
 {
-char c = b[i];
-if (c != '0' && c != '1')
-return (0);
 
+c = b[i];
+if (c != '0' && c != '1')
+{
+return (0);
+}
+else
+{
 num = (num << 1) + (c - '0');
+}
 }
 
 return (num);
